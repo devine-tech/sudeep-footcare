465d16b00c58d43df09d27a19446f73df7d08a46
Ginger Cat <gingercat.331@gmail.com>
2025-03-02 16:46:26 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Disable the react/no-unescaped-entities rule
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",  // Disable this rule
+    },
+  },
 ];
 
 export default eslintConfig;
